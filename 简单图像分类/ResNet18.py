3218fa5071071d609de340d8f4afc761620b3d79
Qinghe Li <derrors@foxmail.com>
2019-12-31 15:36:59 +0800

    Update ResNet18.py

--- 简单图像分类/ResNet18.py
+++ 简单图像分类/ResNet18.py
@@ -1,26 +1,87 @@
 # coding: utf-8
-# @derrors 2019-12-31
+# @derrors 2019-05-08
 #
-# LeNet 网络结构：输入->卷积层->激活->池化->卷积->激活->池化->卷积->激活->池化
-#                    ->全连接->激活->全连接->激活->全连接->激活->输出
-import os
-import csv
+#ResNet18 模型结构：输入->卷积、BN、激活->残差层1->残差层2->残差层3->残差层4->池化->全连接->输出
+#
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
-from PIL import Image
+import torchvision
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR100
 
 
 # 是否使用 GPU 进行处理
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# 定义残差块
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        # 这里采用 conv3x3 -> BN -> ReLU -> conv3x3 -> BN 结构
+        self.left = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+        # 直接传递
+        self.shortcut = nn.Sequential()
+        # 输出维度减半时， shortcut 也引应转换为相应的维度
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+    # 一个残差块的前向传递
+    def forward(self, x):
+        out = self.left(x)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
-def default_loader(path):
-    return Image.open(path).convert('RGB')
+# ResNet18 网络
+class ResNet(nn.Module):
+    def __init__(self, ResidualBlock, classes_num=100):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, 64, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+        # 每个 layer 表示一个残差层
+        self.layer1 = self.make_layer(ResidualBlock, 64, 2, stride=1)
+        self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride=2)
+        self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride=2)
+        self.fc = nn.Linear(512, classes_num)
+    # 创建残差层，每个残差层包含 blocks_num 个残差块
+    def make_layer(self, residual_block, channels, blocks_num, stride):
+        strides = [stride] + [1] * (blocks_num - 1)     # 形式为 [1, 1], [2, 1] 等
+        layers = []
+        for stride in strides:
+            layers.append(residual_block(self.in_channels, channels, stride))
+            self.in_channels = channels
+        return nn.Sequential(*layers)                   # *表示将输入迭代器拆成一个个元素，即顺序链接各层
 
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+    
+def ResNet18():
+    return ResNet(ResidualBlock)
 
 # 数据预处理
 def load_data(batch_size):
@@ -32,87 +93,62 @@ def load_data(batch_size):
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
-    trainset = CIFAR100('../data/CIFAR10', train=True, transform=transform_train, download=True)
-    testset = CIFAR100('../data/CIFAR10', train=False, transform=transform_test, download=True)
+    trainset = CIFAR100('../data/CIFAR100', train=True, transform=transform_train, download=True)
+    testset = CIFAR100('../data/CIFAR100', train=False, transform=transform_test, download=True)
 
     trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=10)
     testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=10)
     return trainset, testset, trainloader, testloader
 
 
-# 定义网络结构
-class LeNet(nn.Module):
-    # 构造函数
-    def __init__(self, classes_num=200):
-        super(LeNet, self).__init__()
-
-        self.conv = nn.Sequential(
-            # 卷积层 in_channels、out_channels、kernel_size、stride、padding
-            #          输入深度     输出深度     卷积核尺寸  滑动步长  补0填充
-            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            nn.Conv2d(6, 16, 5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-
-            nn.Conv2d(16, 120, 5, stride=1, padding=2),
-            nn.ReLU()
-        )
-        self.fc = nn.Sequential(
-            # 全连接层 in_features、out_features 分别表示输入特征数、输出特征数
-            nn.Linear(in_features=8 * 8 * 120, out_features=120),
-            nn.ReLU(),
-            nn.Linear(120, 84),
-            nn.ReLU(),
-            nn.Linear(84, classes_num),
-            nn.Sigmoid()
-        )
-    # 前向传播过程
-
-    def forward(self, x):
-        out = self.conv(x)
-        # reshape 操作
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
-
-
 if __name__ == "__main__":
-    # 参数设置
-    learning_rate = 0.1
-    batch_size = 256
+    # 设置参数
     epoches = 30
+    batch_size = 256
+    learning_rate = 0.1
+    # 加载数据、构建模型、选择损失函数和优化器
+    trainset, testset, trainloader, testloader = load_data(batch_size)
+    resnet = ResNet18().to(device)
+    criterian = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(resnet.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
 
-    # 构建网络模型
-    lenet = LeNet().to(device)
-    trainset, trainloader = load_data(batch_size)
-    # 损失函数：交叉熵
-    criterian = nn.CrossEntropyLoss(reduction='sum')
-    # 优化方法：随机梯度下降 lenet.parameters() 返回 LeNet 网络中可学习的参数  lr为学习率
-    optimizer = optim.SGD(lenet.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
-
-    # 训练网络
     print('Training Starting-------------------------')
     for i in range(epoches):
-        running_loss = 0.
-        running_acc = 0.
-        for (train_img, train_label) in trainloader:
-            train_img, train_label = train_img.to(device), train_label.to(device)
+        # 训练模型
+        train_loss = 0.0
+        train_acc = 0.0
+        resnet.train()
+        for (train_input, train_label) in trainloader:
+            train_input, train_label = train_input.to(device), train_label.to(device)
             # 在训练之前，必须先清零梯度缓存
             optimizer.zero_grad()
-            train_output = lenet(train_img)
+            train_output = resnet(train_input)
             # 计算误差
             loss = criterian(train_output, train_label)
             loss.backward()
             # 参数更新
             optimizer.step()
             # 计算训练过程的损失大小及准确率
-            running_loss += loss.item()
+            train_loss += loss.item()
             _, predict = torch.max(train_output, 1)
             correct_num = (predict == train_label).sum().item()
-            running_acc += correct_num
-        running_loss /= len(trainset)
-        running_acc /= len(trainset)
-        print('[%d/%d] Training_Loss: %.4f, Training_Accuracy: %.2f %% ' % (i + 1, epoches, running_loss, running_acc * 100))
+            train_acc += correct_num
+        train_loss /= len(trainset)
+        train_acc /= len(trainset)
+
+        # 测试模型
+        correct = 0
+        total = 0
+        resnet.eval()
+        # 测试时，无需进行梯度计算与参数更新
+        with torch.no_grad():
+            for (test_input, test_label) in testloader:
+                test_input, test_label = test_input.to(device), test_label.to(device)
+                test_output = resnet(test_input)
+                _, predicted = torch.max(test_output.data, 1)
+                correct += (predicted == test_label).sum().item()
+                total += test_label.size(0)
+                test_acc = correct / total
+
+        print('[%d/%d] Training_Loss: %.4f, Training_Accuracy: %.2f %%  Testing_Accuracy: %.2f %%'
+               % (i+1, epoches, train_loss, train_acc*100, test_acc*100))
